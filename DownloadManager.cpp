e8352e0fe7029f13222b2443e64e22f0e661c231
niemal <nikochat@teiemt.gr>
2014-10-13 22:51:49 +0300

    DownloadManager class methods

--- DownloadManager.cpp
+++ DownloadManager.cpp
@@ -3,7 +3,6 @@
 class QSslError;
 QT_USE_NAMESPACE
 
-
 DownloadManager::DownloadManager() {
     connect(&manager, SIGNAL(finished(QNetworkReply*)),
             SLOT(downloadFinished(QNetworkReply*)));
